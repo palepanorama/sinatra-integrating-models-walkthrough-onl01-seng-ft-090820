bb0b3ad2f2a8371e4a0b09782da11b434a809854
palepanorama <matthew.keith.thomas@gmail.com>
2020-10-21 10:28:00 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,13 +1,20 @@
 require_relative 'config/environment'
 require_relative 'models/text_analyzer.rb'
 
+
 class App < Sinatra::Base
   get '/' do
     erb :index
   end
 
   post '/' do
-    text_from_user = params[:user_text]
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
+    
+    @number_of_words = @analyzed_text.count_of_words
+    @vowels = @analyzed_text.count_of_vowels
+    @consonants = @analyzed_text.count_of_consonants
+    @most_used = @analyzed_text.most_used_letter
+
 
     erb :results
   end
